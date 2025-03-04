10fe9595f18428d3bcc95e3919a3da415291f4de
Shannypoo <shansalazar@dmc.edu.ph>
2025-03-04 15:05:13 +0800

    teasdasd

--- InventoryManagementSystemOfAwesomeness/Form1.Designer.cs
+++ InventoryManagementSystemOfAwesomeness/Form1.Designer.cs
@@ -28,58 +28,82 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-            this.ribbonControl1 = new DevExpress.XtraBars.Ribbon.RibbonControl();
-            this.ribbonPage1 = new DevExpress.XtraBars.Ribbon.RibbonPage();
-            this.ribbonPageGroup1 = new DevExpress.XtraBars.Ribbon.RibbonPageGroup();
-            this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
-            ((System.ComponentModel.ISupportInitialize)(this.ribbonControl1)).BeginInit();
-            this.SuspendLayout();
-            // 
-            // ribbonControl1
-            // 
-            this.ribbonControl1.ExpandCollapseItem.Id = 0;
-            this.ribbonControl1.Items.AddRange(new DevExpress.XtraBars.BarItem[] {
+			this.ribbonControl1 = new DevExpress.XtraBars.Ribbon.RibbonControl();
+			this.ribbonPage1 = new DevExpress.XtraBars.Ribbon.RibbonPage();
+			this.ribbonPageGroup1 = new DevExpress.XtraBars.Ribbon.RibbonPageGroup();
+			this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
+			this.groupControl1 = new DevExpress.XtraEditors.GroupControl();
+			this.simpleButton2 = new DevExpress.XtraEditors.SimpleButton();
+			((System.ComponentModel.ISupportInitialize)(this.ribbonControl1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.groupControl1)).BeginInit();
+			this.groupControl1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// ribbonControl1
+			// 
+			this.ribbonControl1.ExpandCollapseItem.Id = 0;
+			this.ribbonControl1.Items.AddRange(new DevExpress.XtraBars.BarItem[] {
             this.ribbonControl1.ExpandCollapseItem});
-            this.ribbonControl1.Location = new System.Drawing.Point(0, 0);
-            this.ribbonControl1.MaxItemId = 1;
-            this.ribbonControl1.Name = "ribbonControl1";
-            this.ribbonControl1.Pages.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPage[] {
+			this.ribbonControl1.Location = new System.Drawing.Point(0, 0);
+			this.ribbonControl1.MaxItemId = 1;
+			this.ribbonControl1.Name = "ribbonControl1";
+			this.ribbonControl1.Pages.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPage[] {
             this.ribbonPage1});
-            this.ribbonControl1.Size = new System.Drawing.Size(758, 158);
-            // 
-            // ribbonPage1
-            // 
-            this.ribbonPage1.Groups.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPageGroup[] {
+			this.ribbonControl1.Size = new System.Drawing.Size(758, 158);
+			// 
+			// ribbonPage1
+			// 
+			this.ribbonPage1.Groups.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPageGroup[] {
             this.ribbonPageGroup1});
-            this.ribbonPage1.Name = "ribbonPage1";
-            this.ribbonPage1.Text = "ribbonPage1";
-            // 
-            // ribbonPageGroup1
-            // 
-            this.ribbonPageGroup1.Name = "ribbonPageGroup1";
-            this.ribbonPageGroup1.Text = "ribbonPageGroup1";
-            // 
-            // simpleButton1
-            // 
-            this.simpleButton1.Location = new System.Drawing.Point(94, 186);
-            this.simpleButton1.Name = "simpleButton1";
-            this.simpleButton1.Size = new System.Drawing.Size(75, 23);
-            this.simpleButton1.TabIndex = 1;
-            this.simpleButton1.Text = "test";
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(758, 360);
-            this.Controls.Add(this.simpleButton1);
-            this.Controls.Add(this.ribbonControl1);
-            this.Name = "Form1";
-            this.Ribbon = this.ribbonControl1;
-            this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.ribbonControl1)).EndInit();
-            this.ResumeLayout(false);
-            this.PerformLayout();
+			this.ribbonPage1.Name = "ribbonPage1";
+			this.ribbonPage1.Text = "ribbonPage1";
+			// 
+			// ribbonPageGroup1
+			// 
+			this.ribbonPageGroup1.Name = "ribbonPageGroup1";
+			this.ribbonPageGroup1.Text = "ribbonPageGroup1";
+			// 
+			// simpleButton1
+			// 
+			this.simpleButton1.Location = new System.Drawing.Point(23, 38);
+			this.simpleButton1.Name = "simpleButton1";
+			this.simpleButton1.Size = new System.Drawing.Size(75, 23);
+			this.simpleButton1.TabIndex = 1;
+			this.simpleButton1.Text = "test";
+			// 
+			// groupControl1
+			// 
+			this.groupControl1.Controls.Add(this.simpleButton2);
+			this.groupControl1.Controls.Add(this.simpleButton1);
+			this.groupControl1.Location = new System.Drawing.Point(12, 164);
+			this.groupControl1.Name = "groupControl1";
+			this.groupControl1.Size = new System.Drawing.Size(734, 184);
+			this.groupControl1.TabIndex = 3;
+			this.groupControl1.Text = "Test Buttons";
+			// 
+			// simpleButton2
+			// 
+			this.simpleButton2.Location = new System.Drawing.Point(120, 38);
+			this.simpleButton2.Name = "simpleButton2";
+			this.simpleButton2.Size = new System.Drawing.Size(75, 23);
+			this.simpleButton2.TabIndex = 2;
+			this.simpleButton2.Text = "Shan Button";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(758, 360);
+			this.Controls.Add(this.groupControl1);
+			this.Controls.Add(this.ribbonControl1);
+			this.Name = "Form1";
+			this.Ribbon = this.ribbonControl1;
+			this.Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.ribbonControl1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.groupControl1)).EndInit();
+			this.groupControl1.ResumeLayout(false);
+			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
@@ -89,6 +113,8 @@
 		private DevExpress.XtraBars.Ribbon.RibbonPage ribbonPage1;
 		private DevExpress.XtraBars.Ribbon.RibbonPageGroup ribbonPageGroup1;
         private DevExpress.XtraEditors.SimpleButton simpleButton1;
-    }
+		private DevExpress.XtraEditors.GroupControl groupControl1;
+		private DevExpress.XtraEditors.SimpleButton simpleButton2;
+	}
 }
 
