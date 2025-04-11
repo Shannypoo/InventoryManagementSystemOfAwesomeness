34c5a4b871cc56fff51d71447870214072afb1e7
2022-0140 <2022-0140@LAB1-PC17>
2025-04-11 14:33:17 +0800

    Tried Fixing The Login, keyword TRIED

--- LoginForm/Forms/LoginForm.cs
+++ LoginForm/Forms/LoginForm.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -9,7 +10,8 @@ using System.Windows.Forms;
 using DevExpress.XtraEditors;
 using LoginForm.ManagerForm;
 
-namespace LoginForm {
+namespace LoginForm
+{
     public partial class LoginForm : DevExpress.XtraEditors.XtraForm
     {
         public LoginForm()
@@ -22,34 +24,71 @@ namespace LoginForm {
         {
             {
                 string adminemail = "admin@example.com";
-                string adminpass = "admin123";;
+                string adminpass = "admin123"; ;
 
                 string inputEmail = emailTxt.Text.Trim();
                 string inputPassword = passTxt.Text.Trim();
 
-                if (inputEmail == adminemail && inputPassword == adminpass)
-                {
-                    
-                    XtraMessageBox.Show("Login Successful!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                    this.Hide();
-                    EmployeeManagementForm mainForm = new EmployeeManagementForm();
-                    mainForm.Show();
-                }
-                else if (inputEmail == email && inputPassword == pass)
-                {
+                // SQL Server Connection
+                string connectionString = "Data Source=LAB1-PC17;Initial Catalog=Warehouse; Username=sa; Password=123456;";
 
-                    XtraMessageBox.Show("Login Successful!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                    this.Hide();
-                    EmployeeManagementForm mainForm = new EmployeeManagementForm();
-                    mainForm.Show();
-                }
-                else
+                using (SqlConnection conn = new SqlConnection(connectionString))
                 {
-                    XtraMessageBox.Show("Invalid email or password. Please try again.", "Login Failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                   
+                    {
+                        conn.Open();
+                        string query = "SELECT COUNT(*) FROM Users WHERE AccountUsername = @AccountUsername AND AccountPassword = @AccountPassword";
+
+                        using (SqlCommand cmd = new SqlCommand(query, conn))
+                        {
+                            cmd.Parameters.AddWithValue("@AccountUsername", inputEmail);
+                            cmd.Parameters.AddWithValue("@AccountPassword", inputPassword); // Note: Consider hashing for real apps
+
+                            int userCount = (int)cmd.ExecuteScalar();
+
+                            if (userCount > 0)
+                            {
+                                XtraMessageBox.Show("Login Successful!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                                this.Hide();
+                                EmployeeManagementForm mainForm = new EmployeeManagementForm();
+                                mainForm.Show();
+                            }
+                            else
+                            {
+                                XtraMessageBox.Show("Invalid email or password. Please try again.", "Login Failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            }
+                        }
+
+
+                        if (inputEmail == adminemail && inputPassword == adminpass)
+                        {
+
+                            XtraMessageBox.Show("Login Successful!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            this.Hide();
+                            EmployeeManagementForm mainForm = new EmployeeManagementForm();
+                            mainForm.Show();
+                        }
+                        else if (inputEmail == email && inputPassword == pass)
+                        {
+
+                            XtraMessageBox.Show("Login Successful!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            this.Hide();
+                            EmployeeManagementForm mainForm = new EmployeeManagementForm();
+                            mainForm.Show();
+                        }
+                        else
+                        {
+                            XtraMessageBox.Show("Invalid email or password. Please try again.", "Login Failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+
+
+                    }
                 }
-            }
+            
+            
+            }   
         }
-
-
     }
 }
+
+
