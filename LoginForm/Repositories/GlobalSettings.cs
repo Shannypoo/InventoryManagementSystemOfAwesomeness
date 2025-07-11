6142d83e983a8d6326826d07e6cbee2e5ce4c2bd
Shannypoo <shansalazar@dmc.edu.ph>
2025-07-11 22:15:40 +0800

    Added Employee Adding, and made a logger method for easier logging.

--- LoginForm/Repositories/GlobalSettings.cs
+++ LoginForm/Repositories/GlobalSettings.cs
@@ -11,8 +11,8 @@ namespace EmployeeManagementSystem.Repositories
 	{
 		public static string GetConnectionString() 
 		{
-			string sstring = @"DATA Source=LAB1-PC17; Initial Catalog=Warehouse; User ID=sa; Password=123456";
-			//string sstring = @"DATA Source=MYLITTLEWARMACH\SQLEXPRESS; Initial Catalog=Warehouse; Integrated Security=True";
+			//string sstring = @"DATA Source=LAB1-PC17; Initial Catalog=Warehouse; User ID=sa; Password=123456";
+			string sstring = @"DATA Source=MYLITTLEWARMACH\SQLEXPRESS; Initial Catalog=Warehouse; Integrated Security=True";
 			return sstring;
 		}
 
